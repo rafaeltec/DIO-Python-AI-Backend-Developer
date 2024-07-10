61835270473bd202b25ecfce80fe325b27cc83cf
RAFAEL BONDIOLI DE PAULA <rafaeltecsuporte@gmail.com>
2024-07-10 20:33:59 -0300

    u

--- Primeira API com FastAPI, Python e Docker/workout_api/workoutapi/contrib/models.py
+++ Primeira API com FastAPI, Python e Docker/workout_api/workoutapi/contrib/models.py
@@ -1,4 +1,9 @@
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_colum
+
+
+
+
 
 class BaseModel(DeclarativeBase):
-    pass 
+    id: Mapped(UUID) = mapped_colum(PG_UUID(as_uuid=True)) 
+    
