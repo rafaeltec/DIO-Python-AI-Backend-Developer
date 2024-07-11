5257bed55503a6b227f87deec701dfbf4a025077
RAFAEL BONDIOLI DE PAULA <rafaeltecsuporte@gmail.com>
2024-07-11 18:55:08 -0300

    up

--- Primeira API com FastAPI, Python e Docker/workout_api/alembic/env.py
+++ Primeira API com FastAPI, Python e Docker/workout_api/alembic/env.py
@@ -1,6 +1,8 @@
+import asyncio
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
+from sqlalchemy import Connection
+from sqlalchemy.ext.asyncio import async_engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
@@ -52,6 +54,19 @@ def run_migrations_offline() -> None:
     with context.begin_transaction():
         context.run_migrations()
 
+def do_run_migrations(conection: Connection) -> None:
+    context.configure(conection=Connection, target_metadata=target_metadata)
+    with context.begin_transaction():
+        context.run_migrations() 
+        
+async def run_async_migrations():
+    connectable = async_engine_from_config(
+        config.get_section(config.config_ini_section, {}),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )  
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)      
 
 def run_migrations_online() -> None:
     """Run migrations in 'online' mode.
@@ -60,19 +75,7 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
-
-    with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
-
-        with context.begin_transaction():
-            context.run_migrations()
+    asyncio.run(run_async_migrations)
 
 
 if context.is_offline_mode():
