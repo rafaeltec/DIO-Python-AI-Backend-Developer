627848c7dda1b3e5b45f58539d92dcfd01447105
RAFAEL BONDIOLI DE PAULA <rafaeltecsuporte@gmail.com>
2024-07-11 10:04:29 -0300

    up

--- Primeira API com FastAPI, Python e Docker/workout_api/workoutapi/atleta/models.py
+++ Primeira API com FastAPI, Python e Docker/workout_api/workoutapi/atleta/models.py
@@ -1,2 +1,16 @@
-class AtletaMode():
-    pass
+from datetime import datetime
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+from workoutapi.contrib.models import BaseModel
+
+
+class AtletaMode(BaseModel):
+    __tablename__='atletas'
+    pk_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    nome: Mapped[int] = mapped_column(String(50), nullable=False)
+    cpf:Mapped[int] = mapped_column(String(11), nullable=False)
+    idade:Mapped[int] = mapped_column(Integer, nullable=False)
+    peso:Mapped[int] = mapped_column(float, nullable=False)
+    altura:Mapped[int] = mapped_column(Integer, nullable=False)
+    sexo:Mapped[int] = mapped_column(String(1), nullable=False) 
+    created_at: Mapped[datetime] = mapped_column(datetime, nullable=False)
--- Primeira API com FastAPI, Python e Docker/workout_api/workoutapi/contrib/models.py
+++ Primeira API com FastAPI, Python e Docker/workout_api/workoutapi/contrib/models.py
@@ -1,9 +1,11 @@
+from uuid import uuid4
+from sqlalchemy import UUID
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_colum
-
+from sqlalchemy.dialects.postgresql import UUID as PG_UUID
 
 
 
 
 class BaseModel(DeclarativeBase):
-    id: Mapped(UUID) = mapped_colum(PG_UUID(as_uuid=True)) 
+    id: Mapped[UUID] = mapped_colum(PG_UUID(as_uuid=True),default=uuid4, nullable=False) 
     
